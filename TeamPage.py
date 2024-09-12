c2b6960c55989698094b24c467e91b1cd6caf00f
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-09-12 15:11:23 -0400

    Update TeamPage.py
    gofundme_widget = '''
        <div class="gfm-embed" data-url="https://www.gofundme.com/f/empower-706-grey-wolves-football-journey/widget/medium?sharesheet=dashboard&attribution_id=sl:100c727f-d2bf-44ed-bf04-dedbac75b8c1"></div>
         <script defer src="https://www.gofundme.com/static/js/embed.js"></script>
        '''
        components.html(gofundme_widget, height=200)

--- TeamPage.py
+++ TeamPage.py
@@ -304,12 +304,11 @@ elif page == "Sponsorship and Donations":
                     unsafe_allow_html=True)
 
 # Add the GoFundMe widget
-    st.markdown(
-        """
-        <div class="gfm-embed" data-url="https://www.gofundme.com/f/empower-706-grey-wolves-football-journey/widget/medium?sharesheet=dashboard&attribution_id=sl:100c727f-d2bf-44ed-bf04-dedbac75b8c1"></div>
-        <script defer src="https://www.gofundme.com/static/js/embed.js"></script>
-        """,
-        unsafe_allow_html=True)
+    gofundme_widget = '''
+    <div class="gfm-embed" data-url="https://www.gofundme.com/f/empower-706-grey-wolves-football-journey/widget/medium?sharesheet=dashboard&attribution_id=sl:100c727f-d2bf-44ed-bf04-dedbac75b8c1"></div>
+     <script defer src="https://www.gofundme.com/static/js/embed.js"></script>
+    '''
+    components.html(gofundme_widget, height=200)
 
 elif page == "Registration":
     st.markdown("<h1 class='centered-title'>Register Now</h1>", unsafe_allow_html=True)
