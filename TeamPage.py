1177be64be7d01e52fa7f4daaa1cc39e1796d0aa
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-09-15 12:29:03 -0400

    Update TeamPage.py
    width=200

--- TeamPage.py
+++ TeamPage.py
@@ -347,7 +347,7 @@ elif page == "Sponsorship and Donations":
         components.html(gofundme_widget, height=300, width=300)
 
 elif page == "Registration":
-    st.image("Images/706gw_no_bg.png", use_column_width=True)
+    st.image("Images/706gw_no_bg.png", width=200)
     st.markdown("<h1 class='centered-title'>Register Now</h1>", unsafe_allow_html=True)
     st.markdown("""
     <div class="iframe-container">
