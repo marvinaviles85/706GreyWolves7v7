5ed093c6c0f4c216e3245711a6e1eeb45da5093a
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-09-25 10:51:05 -0400

    Update TeamPage.py
    updates to sponsor and reg pages

--- TeamPage.py
+++ TeamPage.py
@@ -394,53 +394,53 @@ elif page == "Sponsorship and Donations":
             st.error(f"Please enter the {payment_method} transaction ID to confirm your payment.")
         else:
             st.success("Thank you for your donation!")
-            save_to_csv(email, first_name, last_name, country, state, is_corporate, donation_amount, payment_method,
+            #save_to_csv(email, first_name, last_name, country, state, is_corporate, donation_amount, payment_method,
                         transaction_id)
 
 
-    def save_to_csv(email, first_name, last_name, country, state, is_corporate, donation_amount, payment_method,
-                    transaction_id):
+   # def save_to_csv(email, first_name, last_name, country, state, is_corporate, donation_amount, payment_method,
+                   # transaction_id):
         # Create a DataFrame with the donation details
-        df = pd.DataFrame({
-            'Email': [email],
-            'First Name': [first_name],
-            'Last Name': [last_name],
-            'Country': [country],
-            'State': [state],
-            'Corporate Donation': [is_corporate],
-            'Donation Amount': [donation_amount],
-            'Payment Method': [payment_method],
-            'Transaction ID': [transaction_id]
-        })
+       # df = pd.DataFrame({
+         #   'Email': [email],
+          #  'First Name': [first_name],
+          #  'Last Name': [last_name],
+         #   'Country': [country],
+         #   'State': [state],
+         #   'Corporate Donation': [is_corporate],
+         #   'Donation Amount': [donation_amount],
+         #   'Payment Method': [payment_method],
+          #  'Transaction ID': [transaction_id]
+       # })
 
         # Save the DataFrame to a CSV file
-        csv_file = 'donations.csv'
-        if os.path.exists(csv_file):
-            df.to_csv(csv_file, mode='a', header=False, index=False)
-        else:
-            df.to_csv(csv_file, index=False)
+        #csv_file = 'donations.csv'
+       # if os.path.exists(csv_file):
+          #  df.to_csv(csv_file, mode='a', header=False, index=False)
+       # else:
+           # df.to_csv(csv_file, index=False)
 
         # Upload the CSV file to GitHub
-        upload_to_github(csv_file)
+        #upload_to_github(csv_file)
 
 
-    def upload_to_github(csv_file):
+   # def upload_to_github(csv_file):
         # Authenticate to GitHub
-        g = Github("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")
+      #  g = Github("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")
 
         # Get the repository
-        repo = g.get_user().get_repo("706GreyWolves7v7")
+       # repo = g.get_user().get_repo("706GreyWolves7v7")
 
         # Read the CSV file content
-        with open(csv_file, 'r') as file:
-            content = file.read()
+       # with open(csv_file, 'r') as file:
+       #     content = file.read()
 
         # Create or update the file in the repository
-        try:
-            contents = repo.get_contents(csv_file)
-            repo.update_file(contents.path, "Update donations", content, contents.sha)
-        except:
-            repo.create_file(csv_file, "Create donations file", content)
+      #  try:
+        #    contents = repo.get_contents(csv_file)
+        #    repo.update_file(contents.path, "Update donations", content, contents.sha)
+        #except:
+          #  repo.create_file(csv_file, "Create donations file", content)
 
 # Registration Page
 if page == "Registration":
@@ -458,32 +458,53 @@ if page == "Registration":
     </div> 
     """, unsafe_allow_html=True)
 
+    # Your Details Section
+    st.header("Your details")
+    email = st.text_input("Email*")
+    first_name = st.text_input("First name*")
+    last_name = st.text_input("Last name*")
+    country = st.selectbox("Country*", ["United States", "Canada", "United Kingdom", "Australia", "Other"])
+    state = st.selectbox("State*", ["Georgia", "California", "New York", "Texas", "Other"])
+    is_corporate = st.checkbox("This is a corporate/organization donation")
+
+    # Payment Method Section
+    st.header("Payment Method")
+    payment_method = st.selectbox("Choose your payment method", ["Venmo", "CashApp"])
+
+    if payment_method == "Venmo":
+        st.write("To complete your donation via Venmo, please [click here](https://venmo.com/code?user_id=3985557692613789993).")
+        st.write("After completing the payment, please enter the transaction ID below.")
+        transaction_id = st.text_input("Venmo Transaction ID")
+    elif payment_method == "CashApp":
+        st.write("To complete your donation via CashApp, please [click here](https://cash.app/$MarvinAviles85).")
+        st.write("After completing the payment, please enter the transaction ID below.")
+        transaction_id = st.text_input("CashApp Transaction ID")
     # Venmo and CashApp Information
-    st.markdown("<h2 class='section-title'>Payment Information</h2>", unsafe_allow_html=True)
-    st.markdown(
-        "<div class='section-content'>Please send your registration payment to our Venmo or CashApp account:</div>",
-        unsafe_allow_html=True)
+    #st.markdown("<h2 class='section-title'>Payment Information</h2>", unsafe_allow_html=True)
+    #st.markdown(
+     #   "<div class='section-content'>Please send your registration payment to our Venmo or CashApp account:</div>",
+      #  unsafe_allow_html=True)
 
     # Create columns for QR codes and buttons
-    col1, col2 = st.columns(2)
+    #col1, col2 = st.columns(2)
 
-    with col1:
-        st.markdown("""
-            <div>
-                <a href="https://venmo.com/code?user_id=3985557692613789993" target="_blank">
-                    <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/Venmo.jpg" width="200">
-                </a>
-            </div>
-        """, unsafe_allow_html=True)
+    #with col1:
+       # st.markdown("""
+       #     <div>
+       #         <a href="https://venmo.com/code?user_id=3985557692613789993" target="_blank">
+       #             <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/Venmo.jpg" width="200">
+       #         </a>
+       #     </div>
+       # """, unsafe_allow_html=True)
 
-    with col2:
-        st.markdown("""
-            <div style="text-align: center;">
-                <a href="https://cash.app/$MarvinAviles85" target="_blank">
-                    <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/CashApp.jpg" width="200">
-                </a>
-            </div>
-        """, unsafe_allow_html=True)
+   # with col2:
+     #   st.markdown("""
+      #      <div style="text-align: center;">
+       #         <a href="https://cash.app/$MarvinAviles85" target="_blank">
+      #              <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/CashApp.jpg" width="200">
+       #         </a>
+       #     </div>
+       # """, unsafe_allow_html=True)
 
 
 if page == "Contact Us":
