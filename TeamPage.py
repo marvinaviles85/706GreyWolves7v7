3a8eb6cdde6f6252931505a01db0fa083feecca6
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-09-26 18:46:22 -0400

    Update TeamPage.py
    None

--- TeamPage.py
+++ TeamPage.py
@@ -41,7 +41,7 @@ custom_css = """
         max-width: 800px;
         margin: 0 auto;
         padding: 20px;
-        background-color: black;
+        background-color: None;
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     }
