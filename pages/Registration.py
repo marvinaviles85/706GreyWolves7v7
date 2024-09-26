ef419c15f1820a0d3e57925e2fa8d4070405ea26
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-09-26 18:52:32 -0400

    Update Registration.py
    remove sidebar collapse

--- pages/Registration.py
+++ pages/Registration.py
@@ -1,23 +1,5 @@
 import streamlit as st
 
-st.set_page_config(initial_sidebar_state="collapsed")
-
-# Inject custom CSS and JavaScript
-st.markdown("""
-    <style>
-        [data-testid='stSidebarNav'] {
-            display: none;
-        }
-    </style>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const sidebar = document.querySelector("[data-testid='stSidebarNav']");
-            if (sidebar) {
-                sidebar.style.display = 'none';
-            }
-        });
-    </script>
-    """, unsafe_allow_html=True)
 
 # Registration Page
 def registration_page():
