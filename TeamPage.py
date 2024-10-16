90b785848d2a8803d3a021c50c6c0b0cfe49cc8c
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2024-10-16 09:20:57 -0400

    Update TeamPage.py
    Adding Fundraiser

--- TeamPage.py
+++ TeamPage.py
@@ -344,6 +344,14 @@ if page == "Home":
                 </a>
             </div>
         """, unsafe_allow_html=True)
+    with col2:
+        st.markdown("""
+            <div style="text-align: center; margin-bottom: 20px;">
+                <a href="https://www.facebook.com/events/2695524287283994/?acontext=%7B%22event_action_history%22%3A[]%7D" target="_blank">
+                    <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/Slim Chickens Fundraiser.jpg" width="200">
+                </a>
+            </div>
+        """, unsafe_allow_html=True)
 
 
  # Add sponsors section
