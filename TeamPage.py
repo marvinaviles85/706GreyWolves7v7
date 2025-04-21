cf7b7d57fbcc60a29e1e5bc0daeee55b8fc7d944
marvinaviles85 <65921282+marvinaviles85@users.noreply.github.com>
2025-04-20 20:27:14 -0400

    Update TeamPage.py
    Commenting out sections of page not needed anymore

--- TeamPage.py
+++ TeamPage.py
@@ -294,21 +294,21 @@ if page == "Home":
 
     carousel(images)
 
-    st.markdown("""
-        <h1 class='centered-title'>Latest News</h1>
-        <p class='subtext'>#MEETUSONTHE50</p>
-    """, unsafe_allow_html=True)
+   # st.markdown("""
+   #     <h1 class='centered-title'>Latest News</h1>
+   #     <p class='subtext'>#MEETUSONTHE50</p>
+   # """, unsafe_allow_html=True)
 
-    col1, col2, col3 = st.columns(3)
+   # col1, col2, col3 = st.columns(3)
 
-    with col1:
-        st.markdown("""
-            <div style="text-align: center; margin-bottom: 20px;">
-                <a href="https://www.facebook.com/profile.php?id=61567362084142" target="_blank">
-                    <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/HandsLeagueLogo.jpg" width="200">
-                </a>
-            </div>
-        """, unsafe_allow_html=True)
+   # with col1:
+       # st.markdown("""
+        #    <div style="text-align: center; margin-bottom: 20px;">
+        #        <a href="https://www.facebook.com/profile.php?id=61567362084142" target="_blank">
+        #            <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/HandsLeagueLogo.jpg" width="200">
+        #        </a>
+        #    </div>
+       # """, unsafe_allow_html=True)
         #st.markdown("""
         #    <div style="text-align: center; margin-bottom: 20px;">
         #        <a href="https://www.instagram.com/706greywolves?fbclid=IwZXh0bgNhZW0CMTAAAR0QOMxQ9T0TWipMlhxmOBQog83Wo3Lb7CaBBI8YY024VM-40ZCvjdPYEZ8_aem_Vxhh_Cw7jD0foFthc2usFA" target="_blank">
@@ -317,14 +317,14 @@ if page == "Home":
         #    </div>
         #""", unsafe_allow_html=True)
         
-    with col2:
-        st.markdown("""
-            <div style="text-align: center; margin-bottom: 20px;">
-                <a href="https://www.facebook.com/photo/?fbid=122181338096177637&set=pb.61555329136562.-2207520000" target="_blank">
-                    <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/NXGNRegionalShowcase.jpg" width="200">
-                </a>
-            </div>
-        """, unsafe_allow_html=True)
+   # with col2:
+      #  st.markdown("""
+       #     <div style="text-align: center; margin-bottom: 20px;">
+       #         <a href="https://www.facebook.com/photo/?fbid=122181338096177637&set=pb.61555329136562.-2207520000" target="_blank">
+       #             <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/NXGNRegionalShowcase.jpg" width="200">
+       #         </a>
+        #    </div>
+       # """, unsafe_allow_html=True)
         #st.markdown("""
         #    <div style="text-align: center; margin-bottom: 20px;">
         #        <a href="https://www.instagram.com/706greywolves?fbclid=IwZXh0bgNhZW0CMTAAAR0QOMxQ9T0TWipMlhxmOBQog83Wo3Lb7CaBBI8YY024VM-40ZCvjdPYEZ8_aem_Vxhh_Cw7jD0foFthc2usFA" target="_blank">
@@ -333,14 +333,14 @@ if page == "Home":
         #    </div>
         #""", unsafe_allow_html=True)
         
-    with col3:
-        st.markdown("""
-            <div style="text-align: center; margin-bottom: 20px;">
-                <a href="https://gofund.me/db7d9283" target="_blank">
-                    <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/CoachCoopsSummerTour.jpg" width="600">
-                </a>
-            </div>
-        """, unsafe_allow_html=True)
+   # with col3:
+        #st.markdown("""
+         #   <div style="text-align: center; margin-bottom: 20px;">
+          #      <a href="https://gofund.me/db7d9283" target="_blank">
+          #          <img src="https://raw.githubusercontent.com/marvinaviles85/706GreyWolves7v7/main/706GWImages/CoachCoopsSummerTour.jpg" width="600">
+           #     </a>
+          #  </div>
+      #  """, unsafe_allow_html=True)
         #st.markdown("""
         #    <div style="text-align: center; margin-bottom: 20px;">
         #        <a href="https://www.instagram.com/706greywolves?fbclid=IwZXh0bgNhZW0CMTAAAR0QOMxQ9T0TWipMlhxmOBQog83Wo3Lb7CaBBI8YY024VM-40ZCvjdPYEZ8_aem_Vxhh_Cw7jD0foFthc2usFA" target="_blank">
